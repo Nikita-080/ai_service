e1c4b2c6edd7534094225bfc3e58f4b5eaa3bfa1
Рябов Никита <n.ryabov@bright-soft.ru>
2025-05-17 20:43:48 +0500

    PEP8 formatting

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
 from ollama import Client
 import json
 
 class Resource():
     def __init__(self):
-        with open("./resources.json", "r") as file:
+        with open('./resources.json', 'r') as file:
             data = json.load(file)
-        self.version = data["version"]
-        self.system_prompt = data["system_prompt"]
+        self.version = data['version']
+        self.system_prompt = data['system_prompt']
+        self.template = data['template']
 
 class Config():
     def __init__(self):
-        with open("./config.json", "r") as file:
+        with open('./config.json', 'r') as file:
             data = json.load(file)
-        self.ollama_host = data["ollama_host"]
-        self.model_name = data["model_name"]
+        self.ollama_host = data['ollama_host']
+        self.model_name = data['model_name']
 
 resource = Resource()
 config = Config()
@@ -26,12 +26,14 @@ app = Flask(__name__)
 
 @app.route('/api/GetAnswer', methods=['GET'])
 def get_answer():
-    question = request.args.get("question")
+    question = request.args.get('question')
+
+    system_message = {'role': 'system', 'content': resource.system_prompt}
+    user_message = {'role': 'user', 'content': question}
+        
     answer = client.chat(config.model_name,   
-                messages=[  
-                    {'role': 'system', 'content': resource.system_prompt},  
-                    {'role': 'user', 'content': question}  
-                ])  
+                         messages=[syste_message, user_message])
+    
     return answer.message.content
 
 @app.route('/api/GetVersion', methods=['GET'])
